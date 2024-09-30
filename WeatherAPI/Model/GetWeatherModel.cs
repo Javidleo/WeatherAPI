fe65eb2585dd997c2c0563a1cc3f142e0198f2d4
mehdijafarimj <mehdij807@gmail.com>
2024-09-30 17:19:07 +0330

    Creating Weather model class for weather controller

---
+++ WeatherAPI/Model/GetWeatherModel.cs
@@ -0,0 +1,32 @@
+﻿using static WeatherAPI.Model.GetWeatherModel;
+
+namespace WeatherAPI.Model;
+public class Location
+{
+    public string Name { get; set; }
+    public string Region { get; set; }
+    public string Country { get; set; }
+    public decimal Lat { get; set; }
+    public decimal Lon { get; set; }
+    public DateTime LocalTime { get; set; }
+}
+public class Current
+{
+    public DateTime Last_Updated { get; set; }
+    public Condition Condition { get; set; }
+}
+public class Condition
+{
+    public string Text { get; set; }
+    public string Icon { get; set; }
+    public int Code { get; set; }
+}
+public class GetWeatherModel
+{
+    public Current Current { get; set; }
+    public Location Location { get; set; }
+    public decimal Wind_Mph { get; set; }
+    public decimal Wind_Kph { get; set; }
+    public int Wind_Degree {  get; set; }
+    public string Wind_Dir { get; set; }
+}
